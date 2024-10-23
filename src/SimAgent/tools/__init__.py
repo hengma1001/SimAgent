cbbf0a2e5040af245a0958628b2c8bf2a00ebe5a
Heng Ma <maheng1991@hotmail.com>
2024-10-23 13:05:52 -0700

    added base md tools

--- src/SimAgent/tools/__init__.py
+++ src/SimAgent/tools/__init__.py
@@ -1,2 +1,2 @@
-from .biotools import write_structure
+from .biotools import fold_sequence, simulate_structure
 from .tools import python_repl, shell_tool, tavily_tool, write_script
--- src/SimAgent/tools/biotools.py
+++ src/SimAgent/tools/biotools.py
@@ -1,14 +1,72 @@
 from pathlib import Path
 from typing import Annotated, Optional
 
+import openmm as omm
+import parmed as pmd
 import torch
 from langchain_core.tools import tool
+from openmm import app
+from openmm import unit as u
 
 from .registry import clear_torch_cuda_memory_callback, register
 
 
 @tool
-def write_structure(
+def simulate_structure(
+    pdb_file: Annotated[str, "3D structure in pdb format"],
+):
+    """
+    Model the molecular structure with molecular dynamics simulation
+
+    Parameters
+    ----------
+    pdb_file : str
+        3D structure in pdb format
+    """
+    top_file, pdb_file = build_top_tleap(pdb_file)
+    top = pmd.load_file(top_file, xyz=pdb_file)
+
+    app.PDBFile.writeFile(top.topology, top.positions, open("input.pdb", "w"))
+    system = top.createSystem(
+        nonbondedMethod=app.PME,
+        nonbondedCutoff=1 * u.nanometer,
+        constraints=app.HBonds,
+        hydrogenMass=4 * u.amu,
+    )
+    barostat = omm.MonteCarloBarostat(1.0 * u.atmosphere, 300 * u.kelvin)
+    system.addForce(barostat)
+    save_omm_system(system, "system.xml")
+
+    integrator = omm.LangevinMiddleIntegrator(300 * u.kelvin, 1 / u.picosecond, 0.004 * u.picoseconds)
+    simulation = app.Simulation(top.topology, system, integrator)
+
+    simulation.context.setPositions(top.positions)
+
+    simulation.minimizeEnergy()
+    simulation.reporters.append(app.DCDReporter("output.dcd", 1000))
+    simulation.reporters.append(
+        app.StateDataReporter(
+            "output.log", 1000, step=True, time=True, potentialEnergy=True, temperature=True, speed=True
+        )
+    )
+    simulation.step(10000)
+
+
+def save_omm_system(system, save_xml):
+    # Serialize the system to an xml file. This is will be necessary for resuming a simulation
+    with open(save_xml, "w") as f:
+        f.write(omm.XmlSerializer.serialize(system))
+
+
+def load_omm_system(system, save_xml):
+    # Load system
+    with open(save_xml, "r") as f:
+        system = omm.XmlSerializer.deserialize(f.read())
+    return system
+
+
+@tool
+def fold_sequence(
     sequence: Annotated[str, "Protein amino acid sequence"],
     output_pdb: Annotated[str, "Path to save the generated protein structure"],
 ):
